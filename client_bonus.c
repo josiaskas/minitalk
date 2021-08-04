8b5af85a5e9a51233a37d2c62844b86573170e07
Josias Kasongo <josiaskas@gmail.com>
2021-08-04 19:54:36 -0400

    adding bonus

---
+++ client_bonus.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkasongo <jkasongo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/29 01:31:36 by jkasongo          #+#    #+#             */
+/*   Updated: 2021/07/01 02:34:43 by jkasongo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/client.h"
+#include <stdio.h>
+
+void	send_byte(int pid, char byte)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		if (byte & (1 << i))
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i++;
+		usleep(25);
+	}
+}
+
+void	signal_recieved_data(int signal)
+{
+	if (signal == SIGUSR1)
+		write(STDOUT_FILENO, "\nServeur : message recu\n", 25);
+}
+
+int	main(int argc, char	*argv[])
+{
+	struct sigaction	sa;
+	int					pid;
+	int					i;
+
+	sa.sa_flags = 0;
+	sa.sa_handler = signal_recieved_data;
+	sa.sa_mask = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		while (argv[2][i] != 0)
+		{
+			send_byte(pid, argv[2][i]);
+			i++;
+		}
+		send_byte(pid, 0);
+	}
+	else
+		write(STDOUT_FILENO, "usage : ./client PID message", 29);
+	return (0);
+}
---
+++ server_bonus.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkasongo <jkasongo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/28 23:23:27 by jkasongo          #+#    #+#             */
+/*   Updated: 2021/07/01 02:43:28 by jkasongo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/server.h"
+
+t_message	g_data;
+
+void	make_byte(int bit)
+{
+	g_data.last_bit = 0;
+	if (g_data.shift_bit < 7)
+	{	
+		g_data.chunk += (bit << g_data.shift_bit);
+		g_data.shift_bit++;
+	}
+	else
+	{
+		g_data.chunk += (bit << 7);
+		g_data.shift_bit = 0;
+		g_data.last_bit = 1;
+	}
+}
+
+void	ft_merge_chunks(int pid)
+{
+	char	data;
+
+	data = g_data.chunk;
+	g_data.chunk = 0;
+	g_data.shift_bit = 0;
+	g_data.buffer[g_data.cursor] = data;
+	g_data.sender_pid = pid;
+	g_data.cursor++;
+}
+
+void	signal_handler(int signal, siginfo_t *info, void *ctx)
+{
+	int		type;
+	int		cursor;
+
+	(void)ctx;
+	type = 0;
+	cursor = g_data.cursor;
+	g_data.ended_message = 0;
+	if (signal == SIGUSR1)
+		type = 1;
+	make_byte(type);
+	if (g_data.last_bit)
+	{
+		ft_merge_chunks(info->si_pid);
+		if ((cursor >= (BUFFER_SIZE - 1)) || (g_data.buffer[cursor] == 0))
+		{
+			write(STDOUT_FILENO, &g_data.buffer, g_data.cursor);
+			if (g_data.buffer[cursor] == 0)
+			{
+				write(STDOUT_FILENO, "\n", 1);
+				g_data.ended_message = 1;
+			}
+			g_data.cursor = 0;
+			g_data.buffer[0] = 0;
+		}
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sa.sa_mask = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_putstr_fd("Server listening on PID:", STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putchar_fd(10, STDOUT_FILENO);
+	while (1)
+	{
+		pause();
+		if (g_data.ended_message && g_data.sender_pid)
+		{
+			kill(g_data.sender_pid, SIGUSR1);
+			g_data.sender_pid = 0;
+		}
+	}
+	return (0);
+}
